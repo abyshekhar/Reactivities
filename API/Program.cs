2adc5e93268887823ae559e7b7294630caafc107
Sudhanshu Shekhar <abyshekhar@gmail.com>
2022-12-10 17:29:05 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(option=>{
+    option.AddPolicy("CorsPolicy",policy=>policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"));
+});
 var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
@@ -37,7 +40,7 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
